7b2fd665892961b739b680116703089d7ea457cc
Almuglezsalcedo <gonzalezsalcedoa@gmail.com>
2019-01-23 11:06:45 +0100

    other changes

--- module-1/lab-mysql/your-code/insert.sql
+++ module-1/lab-mysql/your-code/insert.sql
@@ -20,3 +20,6 @@ INSERT INTO customer (customer_id, customer_name, phone_number,email,address,cit
 VALUES (10001,	'Pablo Picasso',	'+34 636 17 63 82',	'-',	'Paseo de la Chopera, 14',	'Madrid',	'Madrid',	'Spain',	28045, 00006),
 (20001,	'Abraham Lincoln',	'+1 305 907 7086',	'-',	'120 SW 8th St',	'Miami',	'Florida',	'United States',	33130, 00004),
 (30001,	'Napoléon Bonaparte',	'+33 1 79 75 40 00',	'-',	'40 Rue du Colisée',	'Paris',	'Île-de-France',	'France',	75008, 00008 );
+
+ALTER TABLE invoice ADD vin VARCHAR(45);
+ALTER TABLE invoice ADD customer_id INT
